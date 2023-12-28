beabb428112696f586f682fab350c1ee30e4fda4
Chris Britton (RD-US) <chris_britton@trendmicro.com>
2023-12-28 17:25:50 -0600

    Fix get_neighbors abstraction

--- src/main/python/day10.py
+++ src/main/python/day10.py
@@ -53,58 +53,66 @@ class Node(object):
     def is_start(self):
         return self.kind == 'S'
 
-    def get_start_neighbors(self, grid):
-        if not self.neighbors:
-            self.neighbors = []
-            if (grid[self.y - 1][self.x].get_neighbors(grid) and self in
-                    grid[self.y - 1][self.x].get_neighbors(grid)):
-                self.neighbors.append(grid[self.y - 1][self.x])
-            if (grid[self.y][self.x - 1].get_neighbors(grid) and
-                    self in grid[self.y][self.x - 1].get_neighbors(grid)):
-                self.neighbors.append(grid[self.y][self.x - 1])
-            if (grid[self.y + 1][self.x].get_neighbors(grid) and self
-                    in grid[self.y + 1][self.x].get_neighbors(grid)):
-                self.neighbors.append(grid[self.y + 1][self.x])
-            if (grid[self.y][self.x + 1].get_neighbors(grid) and self
-                    in grid[self.y][self.x + 1].get_neighbors(grid)):
-                self.neighbors.append(grid[self.y][self.x + 1])
-        assert len(self.neighbors) == 2
-        return self.neighbors
-
-    def get_neighbors(self, grid):
-        if not self.neighbors:
-            if self.is_n_s():
-                self.neighbors = [grid[self.y - 1][self.x],
-                                  grid[self.y + 1][self.x]]
-            elif self.is_e_w():
-                self.neighbors = [grid[self.y][self.x - 1],
-                                  grid[self.y][self.x + 1]]
-            elif self.is_n_e():
-                self.neighbors = [grid[self.y - 1][self.x],
-                                  grid[self.y][self.x + 1]]
-            elif self.is_n_w():
-                self.neighbors = [grid[self.y - 1][self.x],
-                                  grid[self.y][self.x - 1]]
-            elif self.is_s_w():
-                self.neighbors = [grid[self.y + 1][self.x],
-                                  grid[self.y][self.x - 1]]
-            elif self.is_s_e():
-                self.neighbors = [grid[self.y + 1][self.x],
-                                  grid[self.y][self.x + 1]]
-            elif self.is_start():
-                self.neighbors = self.get_start_neighbors(grid)
-            elif self.is_ground():
-                pass
-            else:
-                raise Exception(f'Unknown kind: {self.kind}')
-        return self.neighbors
-
 
 class Solver(AbstractSolver):
     def __init__(self) -> None:
         super().__init__()
         self.grid = []
         self.start = None
+
+    def get_start_neighbors(self, node):
+        assert node.is_start()
+        if not node.neighbors:
+            node.neighbors = []
+            n_neighbor = self.grid[node.y - 1][node.x]
+            if (self.get_neighbors(n_neighbor) and node in
+                    self.get_neighbors(n_neighbor)):
+                node.neighbors.append(n_neighbor)
+
+            w_neighbor = self.grid[node.y][node.x - 1]
+            if (self.get_neighbors(w_neighbor) and node in
+                    self.get_neighbors(w_neighbor)):
+                node.neighbors.append(w_neighbor)
+
+            s_neighbor = self.grid[node.y + 1][node.x]
+            if (self.get_neighbors(s_neighbor) and node in
+                    self.get_neighbors(s_neighbor)):
+                node.neighbors.append(s_neighbor)
+
+            w_neighbor = self.grid[node.y][node.x + 1]
+            if (self.get_neighbors(w_neighbor) and node in
+                    self.get_neighbors(w_neighbor)):
+                node.neighbors.append(w_neighbor)
+        assert len(node.neighbors) == 2
+        return node.neighbors
+
+    def get_neighbors(self, node):
+        if not node.neighbors:
+            if node.is_n_s():
+                node.neighbors = [self.grid[node.y - 1][node.x],
+                                  self.grid[node.y + 1][node.x]]
+            elif node.is_e_w():
+                node.neighbors = [self.grid[node.y][node.x - 1],
+                                  self.grid[node.y][node.x + 1]]
+            elif node.is_n_e():
+                node.neighbors = [self.grid[node.y - 1][node.x],
+                                  self.grid[node.y][node.x + 1]]
+            elif node.is_n_w():
+                node.neighbors = [self.grid[node.y - 1][node.x],
+                                  self.grid[node.y][node.x - 1]]
+            elif node.is_s_w():
+                node.neighbors = [self.grid[node.y + 1][node.x],
+                                  self.grid[node.y][node.x - 1]]
+            elif node.is_s_e():
+                node.neighbors = [self.grid[node.y + 1][node.x],
+                                  self.grid[node.y][node.x + 1]]
+            elif node.is_start():
+                node.neighbors = self.get_start_neighbors(node)
+            elif node.is_ground():
+                pass
+            else:
+                raise Exception(f'Unknown kind: {node.kind}')
+        return node.neighbors
 
     def init_data(self, data: list[str]) -> None:
         for y in range(len(data)):
@@ -120,43 +128,43 @@ class Solver(AbstractSolver):
         self.init_data(data)
         distance = 1
         prev = self.start
-        curr = prev.get_neighbors(self.grid)[0]
+        curr = self.get_neighbors(prev)[0]
         while curr is not self.start:
             curr.distance = distance
             distance += 1
-            if curr.get_neighbors(self.grid)[0] != prev:
+            if self.get_neighbors(curr)[0] != prev:
                 prev = curr
-                curr = curr.get_neighbors(self.grid)[0]
+                curr = self.get_neighbors(curr)[0]
             else:
                 prev = curr
-                curr = curr.get_neighbors(self.grid)[1]
+                curr = self.get_neighbors(curr)[1]
 
         distance = 1
         prev = self.start
-        curr = prev.get_neighbors(self.grid)[1]
+        curr = self.get_neighbors(prev)[1]
         while curr is not self.start:
             distance += 1
             if distance < curr.distance:
                 curr.distance = distance
-            if curr.get_neighbors(self.grid)[0] != prev:
+            if self.get_neighbors(curr)[0] != prev:
                 prev = curr
-                curr = curr.get_neighbors(self.grid)[0]
+                curr = self.get_neighbors(curr)[0]
             else:
                 prev = curr
-                curr = curr.get_neighbors(self.grid)[1]
+                curr = self.get_neighbors(curr)[1]
 
         answer = 0
         prev = self.start
-        curr = prev.get_neighbors(self.grid)[0]
+        curr = self.get_neighbors(prev)[0]
         while curr is not self.start:
             if curr.distance > answer:
                 answer = curr.distance
-            if curr.get_neighbors(self.grid)[0] != prev:
+            if self.get_neighbors(curr)[0] != prev:
                 prev = curr
-                curr = curr.get_neighbors(self.grid)[0]
+                curr = self.get_neighbors(curr)[0]
             else:
                 prev = curr
-                curr = curr.get_neighbors(self.grid)[1]
+                curr = self.get_neighbors(curr)[1]
         return answer
 
     def solve_part_2(self, data: list[Any]) -> Any:
