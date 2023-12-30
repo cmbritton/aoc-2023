9a46e75cd08355af548888aa0fa3321ce2e8ab01
Chris Britton (RD-US) <chris_britton@trendmicro.com>
2023-12-30 03:23:27 -0600

    Day 11 part 1 solved

--- src/main/python/day11.py
+++ src/main/python/day11.py
@@ -31,24 +31,19 @@ class Solver(AbstractSolver):
                 col_expand_indexes.remove(x) if x in col_expand_indexes else None
             y += 1
 
-        for x in col_expand_indexes:
-            for i, galaxy in enumerate(self.galaxies):
-                if galaxy.x >= x:
-                    # print(f'expanding x for {galaxy} due to empty col {x}')
-                    self.galaxies[i] = Galaxy(galaxy_id=galaxy.galaxy_id, x=galaxy.x+1, y=galaxy.y)
+        for i, galaxy in enumerate(self.galaxies):
+            k = sum([True for x in col_expand_indexes if galaxy.x >= x])
+            self.galaxies[i] = Galaxy(galaxy_id=galaxy.galaxy_id, x=galaxy.x+k, y=galaxy.y)
 
-        for y in row_expand_indexes:
-            for i, galaxy in enumerate(self.galaxies):
-                if galaxy.y >= y:
-                    # print(f'expanding y for {galaxy} due to empty row {y}')
-                    self.galaxies[i] = Galaxy(galaxy_id=galaxy.galaxy_id, x=galaxy.x, y=galaxy.y+1)
+        for i, galaxy in enumerate(self.galaxies):
+            k = sum([True for y in row_expand_indexes if galaxy.y >= y])
+            self.galaxies[i] = Galaxy(galaxy_id=galaxy.galaxy_id, x=galaxy.x, y=galaxy.y+k)
 
     def solve_part_1(self, data: list[Any]) -> Any:
         self.init_data(data)
         answer = 0
         for g1, g2 in itertools.combinations(self.galaxies, 2):
-            d = abs(g1.x - g2.x) + abs(g1.y - g2.y) - 1
-            # print(f'{g1} to {g2}, d={d}')
+            d = abs(g1.x - g2.x) + abs(g1.y - g2.y)
             answer += d
         return answer
 
